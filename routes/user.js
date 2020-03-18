63f585a2d6d85b3cfe28a11c97c2003da4b1c5e1
Sergiu <nixigg@gmail.com>
2020-03-17 23:28:55 -0700

    userSchema and signup

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello from node router!')
-})
+const {signup} = require('../controllers/user');
+
+router.post('/signup', signup)
+
 
 module.exports = router;
