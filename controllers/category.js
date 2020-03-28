c0ab6f5b9c055752d7906769aec3d5d605908772
Sergiu <nixigg@gmail.com>
2020-03-27 23:24:58 -0700

    Category update, remove. Product update

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,21 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler'); 
 
+
+// Category By ID
+exports.categoryById = (req, res, next,id) => {
+  Category.findById(id).exec((err, category) => {
+    if(err || !category) {
+      return res.status(400).json({
+        error: 'Category does not exist'
+      });
+    }
+    req.category = category
+    next();
+  })
+}
+
+// Create Category
 exports.create = (req, res) => {
   const category = new Category(req.body)
   category.save((err, data) => {
@@ -10,5 +25,51 @@ exports.create = (req, res) => {
       });
     }
     res.json({data});
+  })
+};
+
+// READ METHOD
+exports.read = (req, res) => {
+  return res.json(req.category)
+};
+
+// UPDATE
+exports.update = (req, res) => {
+  const category = req.category
+  category.name = req.body.name
+  category.save((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      })
+    }
+    res.json(data)
+  })
+}
+
+// REMOVE
+exports.remove = (req, res) => {
+  const category = req.category
+  category.remove((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      })
+    }
+    res.json({
+      message: "Category deleted"
+    })
+  })
+}
+
+// LIST (Get all categories)
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      })
+    }
+    res.json(data);
   })
 }
--- controllers/product.js
+++ controllers/product.js
@@ -23,7 +23,7 @@ exports.read = (req, res) => {
   return res.json(req.product)
 }
 
-
+// CREATE METHOD      
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm()
   form.keepExtensions = true
@@ -66,7 +66,7 @@ exports.create = (req, res) => {
   })
 }
 
-// REMOVE
+// REMOVE METHOD
 exports.remove = (req, res) => {
   let product = req.product
   product.remove((err, deletedProduct) => {
@@ -77,6 +77,50 @@ exports.remove = (req, res) => {
     }
     res.json({
       "message": "Product deleted successfully"
+    })
+  })
+}
+
+// UPDATE METHOD
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+      })
+    }
+
+    // check for all fields
+    const {name, description, price, category, quantity, shipping} = fields
+    if(!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: "All fields are required"
+      })
+    }
+
+    let product = req.product
+    product = _.extend(product, fields)
+
+    if(files.photo) {
+      //console.log('FILES PHOTO:', files.photo)
+      if(files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image size should be less than 1mb'
+        })
+      }
+      product.photo.data = fs.readFileSync(files.photo.path)
+      product.photo.contentType = files.photo.type
+    }
+
+    product.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        })
+      }
+      res.json(result)
     })
   })
 }
